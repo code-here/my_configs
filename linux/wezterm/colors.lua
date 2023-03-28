46c2de3dba91bcfbb89f222b24c14c5c42c482b6
Abhinav Yadav <abhinavy14@gmail.com>
2023-03-28 12:39:28 +0530

    added wezterm config for linux

---
+++ linux/wezterm/colors.lua
@@ -0,0 +1,49 @@
+return {
+  rosewater = "#F4DBD6",
+  flamingo = "#F0C6C6",
+  pink = "#F5BDE6",
+  mauve = "#C6A0F6",
+  red = "#ED8798",
+  maroon = "#EE99A0",
+  peach = "#F5A97F",
+  yellow = "#EED49F",
+  green = "#A6DA95",
+  teal = "#8BD5CA",
+  sky = "#91D7E3",
+  sapphire = "#7DC4E4",
+  blue = "#8AADF4",
+  lavender = "#B7BDF8",
+  text = "#CAD3F5",
+  subtext1 = "white",
+  subtext0 = "#A5ADCB",
+  overlay2 = "#939AB7",
+  overlay1 = "#8087A2",
+  overlay0 = "#6E738D",
+  surface2 = "#5B6078",
+  surface1 = "#494D64",
+  surface0 = "#363A4F",
+  base = "#020202",
+  mantle = "#1E2030",
+  crust = "#000000",
+}
+
+
+-- local kanagawa = {
+--     foreground = "#dcd7ba",
+--     --background = "#1f1f28",
+--     background = "#000000",
+
+--     cursor_bg = "#c8c093",
+--     cursor_fg = "#c8c093",
+--     cursor_border = "#c8c093",
+
+--     selection_fg = "#c8c093",
+--     selection_bg = "#2d4f67",
+
+--     scrollbar_thumb = "#16161d",
+--     split = "#16161d",
+
+--     ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+--     brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+--     indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+--   }
